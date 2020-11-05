86878730bec48cea77cca01ebc6d1ef23ef2fc6a
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-05 10:04:18 +0000

    OOP four pillars added and init method added

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Classes are essentially blueprints for creating objects with initial values for 
 ## Why
 - Classes are used to group attributes and functions for when multiple objects require them.
 ## 4 pillars
-- Inheritance  (When a class derives from another class)
+- Inheritance  (When a class derives from another class. Eliminating redundant code)
 - Encapsulation (Bundling data and methods that work on data in one unit)
-- Abstraction (Displaying only the essential information to the user)
-- Polymorphism (The ability for an object to take on many forms)
+- Abstraction (Displaying only the essential information to the user. Reducing complexity and increasing re-usability)
+- Polymorphism (Poly - many Morphism - forms) (The ability for an object to change behaviours or attributes / variables)
 
 Class is the main factor that is used to implement any of these pllars
--- dog_class.py
+++ dog_class.py
@@ -9,20 +9,26 @@
 class Dog:
     animal_kind = "Canine"
 
+    def __init__(self, name, colour): # initialising the dog class
+        self.name = name
+        self.colour = colour
+
     # defining class variable
     def bark(self):  # self refers to current class
         return "Woof"
 
 
 # In order to execute a class we have to create an object of this class
-Fido = Dog()  # creating an object of our Dog class
+Fido = Dog("fido", "brown")  # creating an object of our Dog class with required arguments
 
 print(Fido.animal_kind)
 print(Fido.bark())
+print(Fido.name)
+print(Fido.colour)
 
-Fido.animal_kind = "Big Dog" # Changes value of animal kind for Fido object only
-print(Fido.animal_kind)
+# Fido.animal_kind = "Big Dog" # Changes value of animal kind for Fido object only
+# print(Fido.animal_kind)
 
-Lassie = Dog() # Creating an object of our Dog class
-print(Lassie.animal_kind)
-print(Lassie.bark())
+# Lassie = Dog() # Creating an object of our Dog class
+# print(Lassie.animal_kind)
+# print(Lassie.bark())
